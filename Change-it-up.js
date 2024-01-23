f22c781c8bd2316445961b35098ab7f41863a15f
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-23 12:16:54 -0800

    Create Change-it-up.js

---
+++ Change-it-up.js
@@ -0,0 +1,15 @@
+/*
+Create a function that takes a string as a parameter 
+and does the following, in this order:
+
+Replaces every letter with the letter following it in the alphabet
+Makes any consonants lower case
+Makes any vowels capital
+
+So, for example the string "Cat30" would return "dbU30" 
+(Cat30 --> Dbu30 --> dbU30)
+
+Note:
+  the alphabet should wrap around, so Z becomes A
+  in this kata, y isn't considered as a vowel.
+*/
